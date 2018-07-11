d52740d472797f368a83ca3922b1532556f5577d
agonzalez26 <agonzalez26@student.gsu.edu>
2018-07-11 10:47:28 -0400

    RD1
    This is the basic uncompleted templates for the angular project, WeeClo. There are still many things that need to be worked on

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './index/index.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'index', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', redirectTo:'index', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
